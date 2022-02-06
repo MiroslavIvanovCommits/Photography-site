931e9ca10fcceccbf19ba4df391616985fc42839
miro <miro.marvel@protonmail.com>
2022-02-06 22:56:54 +0200

    Add Carousel in Gallery page

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,26 @@
-const toTop = document.querySelector(".to-top");
+// Back to the top button 
+let toTop = document.querySelector(".to-top");
 
 window.addEventListener("scroll", () => {
   if (window.pageYOffset > document.body.scrollHeight / 2.5) {
     toTop.classList.add("active");
   } else {
     toTop.classList.remove("active");
+  }
+})
+
+
+// Owl Carousel
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    920: {
+      items: 3
+    }
   }
 })
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,4 +1,38 @@
 const Gallery = () => {
+    return (
+        <div className="owl-carousel owl-theme">
+            <div className="item">
+                <img src="https://images.pexels.com/photos/917494/pexels-photo-917494.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
+            </div>
+            <div className="item">
+                <img src="https://images.pexels.com/photos/440731/pexels-photo-440731.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
+            </div>
+            <div className="item">
+                <img src="https://images.pexels.com/photos/8241099/pexels-photo-8241099.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" />
+            </div>
+            <div className="item">
+                <img src="https://images.pexels.com/photos/210186/pexels-photo-210186.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
+            </div>
+            <div className="item">
+                <img src="https://images.pexels.com/photos/158063/bellingrath-gardens-alabama-landscape-scenic-158063.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
+            </div>
+            <div className="item">
+                <img src="https://images.pexels.com/photos/33041/antelope-canyon-lower-canyon-arizona.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
+            </div>
+            <div className="item">
+                <img src="https://images.pexels.com/photos/207247/pexels-photo-207247.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
+            </div>
+            <div className="item">
+                <img src="https://images.pexels.com/photos/164025/pexels-photo-164025.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
+            </div>
+            <div className="item">
+                <img src="https://images.pexels.com/photos/131723/pexels-photo-131723.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
+            </div>
+            <div className="item">
+                <img src="https://images.pexels.com/photos/158385/lake-mcdonald-landscape-panorama-sunset-158385.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
+            </div>
+        </div>
+    )
 }
 
 export default Gallery;
