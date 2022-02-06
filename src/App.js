ed4b9a9c6959843a35e5fbc9cb288f11186ae2e7
miro <miro.marvel@protonmail.com>
2022-02-06 13:50:11 +0200

    Add about and gallery pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
 import Dashboard from "./components/Dashboard.js";
+import Gallery from "./components/Gallery.js";
+import About from "./components/About.js";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/about" element={<About />} />
         </Routes>
 
       </main>
