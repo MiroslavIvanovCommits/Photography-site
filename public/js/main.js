4aab709818ec2805868c334b317caa95c3c399f5
miro <miro.marvel@protonmail.com>
2022-02-06 18:29:38 +0200

    Add Back to the top button functionality in the js

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,9 @@
-// Navbar Toggle-button
-// const toggleButton = document.getElementsByClassName('toggle-button')[0]
-// const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toTop = document.querySelector(".to-top");
 
-// toggleButton.addEventListener('click', () => {
-//   navbarLinks.classList.toggle('active')
-// })
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > document.body.scrollHeight / 2.5) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
