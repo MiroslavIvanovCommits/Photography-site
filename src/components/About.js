b083c4b7418784c6cd1de582c0b04506356fa386
miro <miro.marvel@protonmail.com>
2022-02-06 23:28:18 +0200

    Add About page html and css

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,65 @@
 const About = () => {
+    return (
+        <>
+            <section className="bg-light text-center">
+                <div className="container container--narrow">
+                    <h1>About me</h1>
+
+                </div>
+            </section>
+
+            <section>
+                <div className="container">
+                    <h2 className="text-center">Driven by passion</h2>
+                    <div className="split">
+                        <div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure aperiam inventore nemo, asperiores dolorem
+                                nisi voluptate omnis quam sit doloremque expedita architecto dicta, at consequatur.</p>
+                        </div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure aperiam inventore nemo, asperiores dolorem
+                                nisi voluptate omnis quam sit doloremque expedita architecto dicta, at consequatur.</p>
+                        </div>
+                        <div>
+                            <p>Eum quisquam deleniti facere fugit. Vero quidem sunt fuga fugit ea voluptatum placeat ullam eveniet,
+                                minima
+                                sed veritatis laboriosam. Quia cumque est cupiditate quod eum?</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="bg-dark">
+                <div className="container">
+                    <div className="split">
+                        <div>
+                            <h2></h2>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel ipsum dicta fuga vero aliquam! Officiis?</p>
+                        </div>
+                        <div>
+                            <img src="https://images.pexels.com/photos/305086/pexels-photo-305086.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="bg-primary">
+                <div className="container">
+                    <div className="split">
+                        <div>
+                            <img src="https://images.pexels.com/photos/1738636/pexels-photo-1738636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" />
+                        </div>
+                        <div>
+                            <h2>Created with care</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis id hic at vel voluptatem reiciendis
+                                aut,
+                                esse aperiam? Quia aperiam nesciunt sint iste laboriosam quibusdam minus molestias blanditiis esse!</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
 }
 
 export default About;
