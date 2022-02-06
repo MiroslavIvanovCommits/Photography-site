e8d658054e9911211457eee7049974fceb78bfcd
miro <miro.marvel@protonmail.com>
2022-02-06 13:29:00 +0200

    Add header, footer and main pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
+import Dashboard from "./components/Dashboard.js";
+
 function App() {
   return (
     <div className="App">
+
+      {/* <Header /> */}
+
+      <main className="wrapper">
+
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+
+      </main>
+
+      {/* <Footer /> */}
 
     </div>
   );
