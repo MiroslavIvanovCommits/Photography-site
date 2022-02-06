ca51face99a4944089bfc9f35187838851d16455
miro <miro.marvel@protonmail.com>
2022-02-06 13:58:03 +0200

    Add the html for footer

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
 
       </main>
 
-      {/* <Footer /> */}
+      <Footer />
 
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,16 @@
 const Footer = () => {
-}
+    return (
+        <footer>
+            <h3>Get In Touch</h3>
+            <p>Email or call me to set up a consult</p>
+            <p>Email:
+                <strong>contact@ivanphotos.test</strong>
+            </p>
+            <p>Phone:
+                <strong>(+359) 555-5555</strong>
+            </p>
+        </footer>
+    );
+};
 
 export default Footer;
